3a8ef7a858c351d34c1bb1cdfa8c023ecbcf68a9
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:24:57 -0400

    Add Poppy Bubbles/Poppy Bubbles/Poppy_BubblesApp.swift

---
+++ Poppy Bubbles/Poppy Bubbles/Poppy_BubblesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poppy_BubblesApp.swift
+//  Poppy Bubbles
+//
+//  Created by Elliot Williams on 2025-06-04.
+//
+
+import SwiftUI
+
+@main
+struct Poppy_BubblesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
